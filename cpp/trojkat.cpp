62ec0154955963ef95c93d3324fe952bc79e92ce
dudekk17 <anna.dudek@lo1.sandomierz.pl>
2019-09-19 11:40:45 +0200

    trojkaty i kwadraty

---
+++ cpp/trojkat.cpp
@@ -0,0 +1,26 @@
+/*
+ * trojkat.cpp
+ *
+ */
+
+
+#include <iostream>
+#include <cmath>
+using namespace std; 
+
+int main(int argc, char **argv)
+{
+	float a, b, c;
+    cout << "Podaj długości boków trójkąta:";
+    cin >> a >> b >> c;
+    if(a+b>c && a+c>b && b+c>a) {
+       cout << "To trojkat";}
+    else{cout << "otóż nie tym razem";
+    if sqrt(a) + sqrt(b) = sqrt(c)
+    }
+    
+    
+    
+	return 0;
+}
+
