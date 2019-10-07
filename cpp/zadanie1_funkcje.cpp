32c557ceb9f6cedf0ee834b34f4327487ed496d1
dudekk17 <anna.dudek@lo1.sandomierz.pl>
2019-10-07 10:35:54 +0200

    funkcje

---
+++ cpp/zadanie1_funkcje.cpp
@@ -0,0 +1,18 @@
+/*
+ * zadanie1_funkcje.cpp
+ * 
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	int staz = 1;
+    int zarobek = 1000;
+    cout <<
+	return 0;
+}
+
