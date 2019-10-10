87827468846ad21c9f2c76609ca6c4b52944d8c6
dudekk17 <anna.dudek@lo1.sandomierz.pl>
2019-10-10 11:10:39 +0200

    tablice

---
+++ cpp/cw1_funkcje.cpp
@@ -0,0 +1,29 @@
+/*
+ * cw1_funkcje.cpp
+ * 
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+ int staz = 1;
+ int zarobek = 1000;
+ 
+void drukuj(int staz, int zarobek, int lata){
+ cout << "Pracujesz :" << staz << "lat i zarabiasz :" << zarobek << endl;    
+}
+
+void awans(int &staz, int &zarobek, int lata) {
+    
+}
+
+int main(int argc, char **argv)
+{
+    int lata = 0; 
+    cout << "Podaj przewidywany okres pracy:" << endl;
+    cin >> lata;
+	return 0;
+}
+
---
+++ cpp/tablica.cpp
@@ -0,0 +1,27 @@
+/*
+ * tablica.cpp
+ * 
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	int rozmiar = 5;
+    int tablica[19]; // wskazuje pierwsza komorke
+    int i = 0;
+    cout << "Podaj 20 liczb:" << endl;
+   
+    for(i=0; i<rozmiar; i++){
+    cin >> tablica[i];
+   }
+    for(i=0; i<rozmiar; i++){
+    cout << tablica[i] << " ";    
+        
+    }
+	return 0;
+}
+
