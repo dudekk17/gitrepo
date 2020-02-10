6cdc6d2dd0b515853c0347e018c0990c81284409
dudekk17 <anna.dudek@lo1.sandomierz.pl>
2020-02-10 10:34:58 +0100

    systemy16

---
+++ cpp/hex2dec.cpp
@@ -0,0 +1,18 @@
+/*
+ * hex2dec.cpp 
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{   
+    int x = 4;
+    int tab[x];
+    cout << "Podaj czteroznakowy kod szesnastkowy" << endl;
+    cin >> tab[0];
+	return 0;
+}
+
